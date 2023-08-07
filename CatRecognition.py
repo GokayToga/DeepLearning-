b9b72ecd8c7dd89b7f95ebf81c8e73b457116ab9
Gokay Toga <97601328+GokayToga@users.noreply.github.com>
2023-08-07 11:45:42 +0300

    Update CatRecognition.py

---
+++ CatRecognition.py
@@ -0,0 +1,15 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+
+
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
